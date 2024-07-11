a3b16ced33a6c81eaba80d6fc18fc9c236f3a09e
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-07-12 00:37:00 +0300

    Register component done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from "./components/Login"
+import Register from "./components/Register"
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
